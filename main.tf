7937d07fcd44dd32042c8f0ba99a8fbb10cccfec
Ashourito <123368511+Ashourito@users.noreply.github.com>
2024-03-22 11:31:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,11 +77,6 @@ resource "azurerm_network_interface_security_group_association" "example" {
   network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 
-data "azurerm_ssh_public_key" "SshPublicKey" {
-  name                = "terraform"
-  resource_group_name = "azurerm_resource_group"
-}
-
 # Create VM linux
 resource "azurerm_linux_virtual_machine" "vm-linux" {
   name                = "vm-linux"
@@ -93,11 +88,6 @@ resource "azurerm_linux_virtual_machine" "vm-linux" {
     azurerm_network_interface.my_terraform_nic.id,
   ]
 
-admin_ssh_key {
-    username   = "adminuser"
-    public_key = data.azurerm_ssh_public_key.SshPublicKey.public_key
-  }
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -108,5 +98,26 @@ admin_ssh_key {
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
+  }
+
+# Add the admin SSH key block
+  admin_ssh_key {
+    username = "adminuser" # Make sure this matches your admin username
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwrO7pZQaaBl+p2W2JK5RqsZbD5Ep7sEh2uV52F2QJxcvKAbQ11jiyPTypzN7S19ZLwmbvp5lDSKA6aRXxa00Yp7R4N4uO/tCzp+Mf/SGtrreS6DIs/iGv/uChJTpMkcoD0vnwXPv/lChv1Mx4jCW2oQZfcBjU+Dr0u5C1eC2xGnrSPcLWM8DGKBpbWJgY1eSXR0+jPiaQIm/GqyuBzaHfjeecNSsTSPWk/1qVLerhGxZnM03F8FvQMh1Ayt4CctUeCgTxOgYVxq1AXNN7euD3aVQwizBPrRqdSOPP9DCkckUE7VPbJV1xMktffgaLiRR8ZFcZvrkN0oWWQHhbh1AkDTRqPLvJOAE3LzhWRdW3mmlO4YaJRVS+OdqkBNtA7ioPAS5Jbm6pc5GCdm0RC3X59VyE0eNOoh53UN3ithmmQr/beVE/8TGnAxEefBuVykAteEZutQRAZs4WW47uw5f/LNRf0BV5gGWpdKeA3K367Wgp2SW2BSZ/+gjXv6KNZfc= azureuser@Terraform" # Replace with your actual public key content
+  }
+
+}
+
+# Add storage account
+
+resource "azurerm_storage_account" "vm-linux" {
+  name                     = "csb1003200354c76999fe35"
+  resource_group_name      = azurerm_resource_group.vm-linux.name
+  location                 = azurerm_resource_group.vm-linux.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
   }
 }
